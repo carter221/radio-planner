cf7b4b6dc58e062dc3e13ba4ca9b493875774b30
SEITCHA Judicael <sjudicael3@gmail.com>
2024-12-03 22:33:09 +0000

    switch with transition de liquidsoap mis en place

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -38,59 +38,18 @@ def generate_liquidsoap_config():
         end_time__gt=current_time
     ).first()
 
-    liquidsoap_path = '/usr/src/app/media/radio.liq'  # Fichier Liquidsoap
+    config_path = '/usr/src/app/media/playlist.liq'  # Fichier de configuration Liquidsoap
     base_path = '/home/mediauser/songs/'  # Chemin des chansons
-
-    # Configuration par défaut (aucun programme actif)
-    default_config = '''
-    # Charger la chanson et la faire tourner en boucle manuellement
-    backup = fallback([single("/home/mediauser/songs/song.mp3"), fallback([])])
-
-    # Configurer la source en direct depuis Icecast
-    live = mksafe(fallback(track_sensitive = false, [input.http("http://radio.eglisecdau.com:8000/live"), backup]))
-
-    # Créer un fallback entre la source live et le backup
-    radio = fallback(track_sensitive=false, [live, backup])
-
-    # Configurer la sortie vers Icecast
-    output.icecast(
-    %mp3,
-    host="radio.eglisecdau.com",
-    port=8000,
-    password="601Blaise@",
-    mount="/stream",
-    radio
-    )
-    '''
 
     if current_schedule and current_schedule.song.file_path:
         song_file_name = os.path.basename(current_schedule.song.file_path.name)
         absolute_song_path = os.path.join(base_path, song_file_name)
 
-        config = f'''
-        # Charger la chanson et la faire tourner en boucle manuellement
-        backup = fallback([single("{absolute_song_path}"), fallback([])])
-
-        # Configurer la source en direct depuis Icecast
-        live = mksafe(fallback(track_sensitive = false, [input.http("http://radio.eglisecdau.com:8000/live"), backup]))
-
-        # Créer un fallback entre la source live et le backup
-        radio = fallback(track_sensitive=false, [live, backup])
-
-        # Configurer la sortie vers Icecast
-        output.icecast(
-        %mp3,
-        host="radio.eglisecdau.com",
-        port=8000,
-        password="601Blaise@",
-        mount="/stream",
-        radio
-        )
-        '''
+        config = f'{absolute_song_path}\n'
         program_identifier = absolute_song_path
     else:
-        # Si aucun programme actif, utilisez la configuration par défaut
-        config = default_config
+        # Si aucun programme actif, utilisez une chanson par défaut
+        config = '/home/mediauser/songs/song.mp3'
         program_identifier = "default"
 
     # Générer un hash unique pour le programme actuel
@@ -106,9 +65,9 @@ def generate_liquidsoap_config():
 
     # Si le programme a changé, mettre à jour le fichier et sauvegarder l'état
     try:
-        with open(liquidsoap_path, 'w') as f:
+        with open(config_path, 'w') as f:
             f.write(config)
         save_program_state(current_program_hash)
-        print(f"Fichier {liquidsoap_path} mis à jour avec succès.")
+        print(f"Fichier {config_path} mis à jour avec succès.")
     except Exception as e:
-        print(f"Erreur lors de la mise à jour de {liquidsoap_path} : {e}")
+        print(f"Erreur lors de la mise à jour de {config_path} : {e}")
